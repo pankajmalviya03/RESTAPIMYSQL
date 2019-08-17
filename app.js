024e12b14287eecf8d99ddaa1b9e0d282c487cd0
pankajmalviya02 <pmalviya@gmail.com>
2019-08-17 23:00:13 +0530

    setup app for heroku

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var http = require("http");
 var express = require('express');
 var mysql      = require('mysql');
 var bodyParser = require('body-parser');
+const port=process.env.PORT||3000
 var app = express();
 const route=require("./routes/route")
 //var connection=require("./utils/connection")
@@ -10,7 +11,7 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 }));
 app.use("/",route)
-var server = app.listen(3000,  "localhost",  ()=> {
+var server = app.listen(port,  "localhost",  ()=> {
   var host = server.address().address
   var port = server.address().port
   console.log("Example app listening at http://%s:%s", host, port)
